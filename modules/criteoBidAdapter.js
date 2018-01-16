513d6655fa251b76361e737f75edbac7390bb755
Nicolas Faure <n.faure@criteo.com>
2018-01-16 10:59:13 +0100

    Add support for publisherSubId

--- modules/criteoBidAdapter.js
+++ modules/criteoBidAdapter.js
@@ -68,8 +68,8 @@ var CriteoAdapter = function CriteoAdapter() {
               var w = parseInt(sizeString.substring(0, xIndex));
               var h = parseInt(sizeString.substring(xIndex + 1, sizeString.length))
               return new Criteo.PubTag.DirectBidding.Size(w, h);
-            }
-            )
+            }),
+            bid.params.publisherSubId
           )
         );
 
